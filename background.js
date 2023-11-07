2c9fd66e58ef004abd74fe7a6e7b2539470903ed
Jeffrey Ke <jeffke613@gmail.com>
2023-11-06 16:27:44 -0800

    try 2

--- background.js
+++ background.js
@@ -5,29 +5,29 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   
     // Create an alarm so we have something to look at in the demo
     await chrome.alarms.create('check-prices-alarm', {
-      delayInMinutes: 30,
-      periodInMinutes: 30
+      delayInMinutes: 2,
+      periodInMinutes: 2
     });
   });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-fetch('http://127.0.0.1:5000/data', {method:'GET',mode:'no-cors'})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.text(); 
-})
-.then(data => {
-    console.log(data);
-    const jsonData = JSON.parse(data);
-    chrome.storage.local.set(jsonData).then(() => {
-        console.log("Value is set");
-      });
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+    fetch('http://127.0.0.1:5000/data', {method:'GET',mode:'no-cors'})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text(); 
+    })
+    .then(data => {
+        console.log(data);
+        const jsonData = JSON.parse(data);
+        chrome.storage.local.set(jsonData).then(() => {
+            console.log("Value is set");
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
